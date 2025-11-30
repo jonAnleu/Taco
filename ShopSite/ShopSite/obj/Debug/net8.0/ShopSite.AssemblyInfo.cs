361e21f42eb4f14d0af6b0d40a63bdf34d891519
Jon Anleu <96894253+jonAnleu@users.noreply.github.com>
2025-11-30 06:03:58 -0600

    font and logo set in

--- ShopSite/ShopSite/obj/Debug/net8.0/ShopSite.AssemblyInfo.cs
+++ ShopSite/ShopSite/obj/Debug/net8.0/ShopSite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40d998b61525ab410b49c2e08c584175e00900a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f76254e19017015e21f5083bf1c484bda22877ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
