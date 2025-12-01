0b3a48ba7ec73610b1576c7af58e8c3c80024f58
Jon Anleu <96894253+jonAnleu@users.noreply.github.com>
2025-12-01 05:10:29 -0600

    new resized taquero logo added

--- ShopSite/ShopSite/obj/Debug/net8.0/ShopSite.AssemblyInfo.cs
+++ ShopSite/ShopSite/obj/Debug/net8.0/ShopSite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4029ce2bc0750fb96496ab2bad5e8c12a1c95f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9945d30bf5abd3bcdfa086a779a2d2c4d676ea60")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
